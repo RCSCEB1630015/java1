5e2897c08cb99b5bb1b75f4b211171bae8ef6413
Ivashka <example@mail.com>
2020-01-24 00:30:33 +0300

    java 2lesson

--- js/script.js
+++ js/script.js
@@ -12,14 +12,35 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов?", ""),
+		b = +prompt("Во сколько это обойдется?", "");
 
-alert(appData.budget / 30);
+	if (typeof(a) === 'string' &&  typeof(a) != null && typeof(b) != null
+		&& a !=='' && b !=='' && a.length < 50); {
+		console.log("done");
+		appData.expenses[a] = b;
+	} /* else {
+		
+	} */
+	
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+	console.log("Ваш доход минимален")
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 1000) {
+	console.log("Средний уровень достатка")
+} else if(appData.moneyPerDay > 1000) {
+	console.log("Высокий достаток")
+} else {
+	console.log("ошибка")
+};
+
+
 
 
